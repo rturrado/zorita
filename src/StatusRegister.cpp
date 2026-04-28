b0151ad29fd765987e1e54f9ad805163c141e793
rturrado <rturrado@gmail.com>
2026-04-28 19:48:17 +0200

    Minor change to StatusRegister.cpp
    Move to_uint16 method around.

--- src/StatusRegister.cpp
+++ src/StatusRegister.cpp
@@ -10,6 +10,10 @@ StatusRegister::StatusRegister() : data_{0} {}
 
 StatusRegister::StatusRegister(uint16_t value) : data_{value} {}
 
+uint16_t StatusRegister::to_uint16() const {
+  return static_cast<uint16_t>(data_.to_ulong());
+}
+
 bool StatusRegister::zf() const { return data_[zero_flag_index]; }
 
 bool StatusRegister::nf() const { return data_[negative_flag_index]; }
@@ -17,10 +21,6 @@ bool StatusRegister::nf() const { return data_[negative_flag_index]; }
 bool StatusRegister::cf() const { return data_[carry_flag_index]; }
 
 bool StatusRegister::of() const { return data_[overflow_flag_index]; }
-
-uint16_t StatusRegister::to_uint16() const {
-  return static_cast<uint16_t>(data_.to_ulong());
-}
 
 void StatusRegister::set_st(uint16_t value) { data_ = value; }
 
